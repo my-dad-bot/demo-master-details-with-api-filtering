5c0d70d0bcc58121b2b066c9725bcae18d6fea1e
mabrurChowdhury <mabrur.chowdhury@shohoz.com>
2021-11-06 02:25:21 +0600

    adapter onclick to details view

--- app/src/main/java/com/master_details_view_api_filter/activities/master/MasterRecyclerAdapter.kt
+++ app/src/main/java/com/master_details_view_api_filter/activities/master/MasterRecyclerAdapter.kt
@@ -1,13 +1,16 @@
 package com.master_details_view_api_filter.activities.master
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.master_details_view_api_filter.R
+import com.master_details_view_api_filter.activities.food_details.ui.FoodDetailsActivity
 import com.master_details_view_api_filter.base.data.remote.FilteredMeal
 import com.master_details_view_api_filter.utils.load
 
@@ -26,6 +29,12 @@ class MasterRecyclerAdapter(
     override fun onBindViewHolder(holder: MealViewHolder, position: Int) {
         holder.textView.text = foodlist.meals[position].strMeal
         holder.imageView.load(foodlist.meals[position].strMealThumb)
+        holder.cardView.setOnClickListener {
+            Intent(context, FoodDetailsActivity::class.java).apply {
+                putExtra("meal_id", foodlist.meals[position].idMeal)
+                context.startActivity(this)
+            }
+        }
 
     }
 
@@ -34,6 +43,7 @@ class MasterRecyclerAdapter(
     class MealViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.findViewById(R.id.imageFood)
         val textView: TextView = itemView.findViewById(R.id.textView)
+        val cardView: CardView = itemView.findViewById(R.id.card_view)
     }
 
 }
